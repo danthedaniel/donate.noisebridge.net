741b78c25da09483f6f03a29e7dc3c9a1a1ffbe0
Daniel Angell <me@danangell.com>
2025-12-21 19:45:28 -0800

    Always show stacktrace

--- src/views/error.tsx
+++ src/views/error.tsx
@@ -1,7 +1,6 @@
 // biome-ignore lint/correctness/noUnusedImports: Html is used by JSX
 import Html from "@kitajs/html";
 import { Layout } from "~/components/layout";
-import config from "~/config";
 
 export type ErrorPageProps = {
   error: Error;
@@ -14,28 +13,19 @@ export function ErrorPage({ error, isAuthenticated }: ErrorPageProps) {
       <div class="error-page">
         <h1 class="error-heading">Oops! Something went wrong</h1>
 
-        {!config.production ? (
-          <div class="error-details">
-            <div class="error-message">
-              <h2>Error Message:</h2>
-              <pre>{error.message}</pre>
-            </div>
+        <div class="error-details">
+          <div class="error-message">
+            <h2>Error Message:</h2>
+            <pre>{error.message}</pre>
+          </div>
 
-            {error.stack && (
-              <div class="error-stack">
-                <h2>Stack Trace:</h2>
-                <pre>{error.stack}</pre>
-              </div>
-            )}
-          </div>
-        ) : (
-          <div class="error-production">
-            <p class="error-description">
-              We're sorry, but something went wrong on our end. Please try again
-              later.
-            </p>
-          </div>
-        )}
+          {error.stack && (
+            <div class="error-stack">
+              <h2>Stack Trace:</h2>
+              <pre>{error.stack}</pre>
+            </div>
+          )}
+        </div>
       </div>
     </Layout>
   );
